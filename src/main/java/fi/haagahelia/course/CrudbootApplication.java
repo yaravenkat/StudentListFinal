4862d0ecd4bcf44b16afd03694e6d64b249a15ed
Yara Venkateswarlu <yara.venky@gmail.com>
2024-02-03 11:42:08 +0800

    crud oparation

--- src/main/java/fi/haagahelia/course/CrudbootApplication.java
+++ src/main/java/fi/haagahelia/course/CrudbootApplication.java
@@ -44,6 +44,7 @@ public class CrudbootApplication {
 			// Create users with BCrypt encoded password (user/user, admin/admin)
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$08$bCCcGjB03eulCWt3CY0AZew2rVzXFyouUolL5dkL/pBgFkUH9O4J2", "ADMIN");
+			
 			urepository.saveAll(Arrays.asList(user1, user2));
 		};
 	}
